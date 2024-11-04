fa9585d27fb6b53ace51ddd1cf0fb2ab882c5b87
Divindivine <divinparakalayil@gmail.com>
2024-11-04 23:59:50 +0530

    4/11/2024

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -5,24 +5,32 @@ function CartItems({ data, cartItems }: CartItemsPropType) {
   // const [productsItems, setproductsItems] = useState<EachProductType[]>([])
   // const products: EachProductType[] = [];
   // let prev:EachProductType[] = []
-  console.log(cartItems)
-//   if (cartItems) {
-//     products.length = 0;
-//     cartItems.map((id: number) => {
-//       data.map((product: EachProductType) => {
-//         if (product.id === id) {
-//           products.push(product);
-//         }
-//       });
-//     });
-//   }
+  console.log(cartItems);
+  //   if (cartItems) {
+  //     products.length = 0;
+  //     cartItems.map((id: number) => {
+  //       data.map((product: EachProductType) => {
+  //         if (product.id === id) {
+  //           products.push(product);
+  //         }
+  //       });
+  //     });
+  //   }
 
   return (
-    <div
-      className="w-[400px] h-[600px] bg-white z-10 absolute flex flex-col text-black ">HAIash
-      {/* {cartItems && cartItems.map((product: EachProductType) => (
-        <div key={product.id} className="text-[20px] text-black">hI{product.id}</div>
-      ))} */}
+    <div className="w-[400px] h-[600px] bg-white z-10 top-[20px] absolute flex flex-col text-black p-[10px]">
+      {cartItems &&
+        cartItems.map((product: EachProductType) => (
+          <div key={product.id} className="flex w-100 justify-between">
+            <div className="w-[50px] h-[50px]" style={{
+                backgroundImage: `url(${product.image})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}></div>
+              
+          </div>
+        ))}
     </div>
   );
 }
